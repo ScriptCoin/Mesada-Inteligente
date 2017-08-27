785980de44e6a4c1ebe5df62a732c3cca1cdba91
Guilherme Ventura <guilhermeventura2@gmail.com>
2017-08-27 03:08:15 -0300

    Localization, transferência

--- dashboard-pai/app/page-recharge/recharge.controller.js
+++ dashboard-pai/app/page-recharge/recharge.controller.js
@@ -6,19 +6,32 @@
   function RechargeController(RestService) {
     var $ctrl = this;
 
+    $ctrl.mesada = 300;
+    $ctrl.score = 819;
+
     $ctrl.prize = null;
     $ctrl.formatDate = formatDate;
+    $ctrl.transfer = transfer;
+    $ctrl.scoreToMoney = scoreToMoney;
 
     function init() {
-      fetchData();
+      $ctrl.value = scoreToMoney($ctrl.score);
     }
 
     function fetchData() {
-      RestService.getPrize(2).then(setPrize);
     }
 
     function setPrize(prize) {
       $ctrl.prize = prize;
+    }
+
+    function scoreToMoney(score) {
+      return ($ctrl.mesada / 1000) * score;
+    }
+
+    function transfer(value) {
+      RestService.addFunds(1, value)
+        .then(console.log);
     }
 
     init();
