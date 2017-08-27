9c609419586cdf4a539541afa60ad712381457ee
Guilherme Ventura <guilhermeventura2@gmail.com>
2017-08-27 05:26:09 -0300

    Adiciona passo 0 nas atividades

--- dashboard-pai/app/page-tasks/tasks.controller.js
+++ dashboard-pai/app/page-tasks/tasks.controller.js
@@ -2,23 +2,27 @@
   angular.module('aboilerplate')
     .controller('TasksController', TasksController);
 
-  TasksController.$inject = ['RestService', 'FirebaseService'];
-  function TasksController(RestService, FirebaseService) {
+  TasksController.$inject = ['$window', '$q', 'RestService', 'FirebaseService'];
+  function TasksController($window, $q, RestService, FirebaseService) {
     var $ctrl = this;
 
     $ctrl.templates = null;
     $ctrl.todayTasks = null;
+    $ctrl.showChecklist = null;
     $ctrl.getTemplateTasksSummary = getTemplateTasksSummary;
     $ctrl.selectTemplate = selectTemplate;
 
     function init() {
       fetchData();
+
+      $ctrl.showChecklist = shouldShowChecklist();
     }
 
     function fetchData() {
       $ctrl.isLoading = true;
       $ctrl.todayTasks = FirebaseService.getTasks(1, 2);
-      $ctrl.todayTasks.$loaded()
+
+      $q.all([$ctrl.todayTasks.$loaded(), loadTemplates()])
         .then(() => $ctrl.isLoading = false);
     }
 
@@ -31,7 +35,8 @@
     }
 
     function selectTemplate(template) {
-      setTasks(angular.copy(template.tasks));
+      $ctrl.showChecklist = true;
+      setShouldShowChecklist(true);
     }
 
     function setTasks(tasks) {
@@ -41,6 +46,14 @@
     init();
 
     // helpers
+    function shouldShowChecklist() {
+      return $window.localStorage.getItem('shouldShowChecklist');
+    }
+
+    function setShouldShowChecklist(show) {
+      return $window.localStorage.setItem('shouldShowChecklist', show);
+    }
+
     function getTemplateTasksSummary(template) {
       var grouppedTasks = template.tasks.groups
         .map(g => g.tasks)
